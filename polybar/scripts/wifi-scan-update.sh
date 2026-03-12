75972768143bf3e67bf74bdb76a580247b2da9dd
Amber Sweet <Hexddesign@gmail.com>
2026-03-12 13:27:09 -0400

    Delete polybar/scripts/wifi-scan-update.sh

--- polybar/scripts/wifi-scan-update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Update the wifi-results variable in Eww
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-WIFI_LIST="$("$SCRIPT_DIR/wifi-list.sh")"
-eww update wifi-results="$WIFI_LIST"
