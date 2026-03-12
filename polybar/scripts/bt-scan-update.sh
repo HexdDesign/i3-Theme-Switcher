58be3e83b3c002b6ebeec34930646d3282c1b122
Amber Sweet <Hexddesign@gmail.com>
2026-03-12 12:49:15 -0400

    Delete polybar/scripts/bt-scan-update.sh

--- polybar/scripts/bt-scan-update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Update the bt_results variable in Eww
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BT_LIST="$("$SCRIPT_DIR/bt-list.sh")"
-eww update bt_results="$BT_LIST"
